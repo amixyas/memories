f8007831ec85fea685da7b7576f4621ff667bd6b
amixyas <i.amirat@esi-sba.dz>
2022-11-16 10:03:18 +0100

    update index.js with routes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
+app.use('/posts', postRoutes);
 
 // https://www.mongodb.com/cloud/atlas
 
